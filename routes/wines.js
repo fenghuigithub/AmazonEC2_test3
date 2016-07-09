037bd0c9f07726f121403bffdf6ba20194948ac3
hfeng49 <hfeng49@gatech.edu>
2016-07-09 03:29:16 -0400

    change to URL

--- routes/wines.js
+++ routes/wines.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'); 
-mongoose.connect("mongodb://fengh:app@ec2-54-173-25-145.compute-1.amazonaws.com:27017/dummyDB"); 
+mongoose.connect("mongodb://fengh:app@ec2-54-173-25-145.compute-1.amazonaws.com:27017/AmazonEC2_test3"); 
 var db=mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
 db.once('open',function(){
